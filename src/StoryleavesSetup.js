d90bf7c9705c61cab6304846ea9b125622b1b71f
Amelia Mendicino <amelia.mendicino@gmail.com>
2017-07-02 20:13:39 -0600

    Add StoryleavesSetup Placeholder
    A placeholder component for the Setup portion of the game; currently
    just displays a string so we can verify that it loads correctly after
    we finished creating our deck.

---
+++ src/StoryleavesSetup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//import { ListGroup, ListGroupItem } from "react-bootstrap";
+//import { GameState } from "./GameState";
+
+class StoryleavesSetup extends Component {
+  render() {
+    return <div>This is the Setup Component</div>;
+  }
+}
+
+export default StoryleavesSetup;
