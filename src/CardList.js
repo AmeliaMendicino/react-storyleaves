f075e4bb2c72f47407279be25dae25610780022e
Amelia Mendicino <amelia.mendicino@gmail.com>
2017-07-12 22:49:24 -0600

    Handle Active Card
    The CardList is now handling which card is the currently active card
    that's being edited. This is much easier than trying to have the Card
    component switching itself onClick. MUCH better.

--- src/CardList.js
+++ src/CardList.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import CardForm from './CardForm';
 
 class CardList extends Component {
+  constructor() {
+    super();
+
+    this.state = { canEdit: true, activeCard: {} };
+  }
+
   render() {
     return (
       <div>
-        {this.props.cards.map(card =>
-          <Card key={card.id} {...card} updateCard={this.props.updateCard} />
-        )}
+        {this.props.cards.map(card => {
+          let CardComponent = Card;
+          if (this.state.activeCard === card) {
+            CardComponent = CardForm;
+          }
+          return (
+            <CardComponent
+              key={card.id}
+              {...card}
+              updateCard={this.props.updateCard}
+              moveCard={this.props.moveCard}
+              toggleCard={() => this._toggleActiveCard(card)}
+            />
+          );
+        })}
       </div>
     );
+  }
+
+  _toggleActiveCard(card) {
+    if (card === this.state.activeCard) {
+      card = {};
+    }
+    this.setState({ activeCard: card });
   }
 }
 
