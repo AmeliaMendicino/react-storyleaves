77e5d487376b1cda254e027153220f76954aab5c
Amelia Mendicino <amelia.mendicino@gmail.com>
2017-07-12 22:29:24 -0600

    Break out CardForm Component
    Split the Card component into a simple display component and a form
    component for editing the card.

---
+++ src/CardForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class CardForm extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      name: props.name,
+      type: props.type,
+      number: props.number
+    };
+
+    this.focus = this.focus.bind(this);
+  }
+
+  focus() {
+    // Explicitly focus the text input using the raw DOM API
+    this.textInput.focus();
+  }
+
+  componentDidMount() {
+    this.focus();
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <input
+          defaultValue={this.props.number}
+          onBlur={event => {
+            if (this.props.number !== Number(event.target.value)) {
+              console.log(this.props.number);
+              console.log(event.target.value);
+              console.log(event.relatedTarget);
+              //this.props.moveCard(this.props.id, event.target.value);
+            }
+          }}
+          type="number"
+        />
+        <input
+          defaultValue={this.props.name}
+          placeholder="Name"
+          ref={input => (this.textInput = input)}
+          onChange={event =>
+            this.props.updateCard(this.props.id, event.target.value)}
+          onKeyDown={event => {
+            if (event.keyCode === 13) {
+              event.preventDefault();
+              this.props.toggleCard();
+            }
+          }}
+        />
+        [{this.props.type}]
+      </div>
+    );
+  }
+}
+
+export default CardForm;
