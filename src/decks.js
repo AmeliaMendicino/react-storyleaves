e1ecd338498a83cf935671cd8ca5e357ec0063e8
Amelia Mendicino <amelia.mendicino@gmail.com>
2017-07-15 23:24:33 -0600

    Don't use card.id's
    Since we're not using the card id properties anymore, removing them
    from the example decks.

--- src/decks.js
+++ src/decks.js
@@ -7,241 +7,201 @@ export const decks = [
     cardTypes: ['Character', 'Aspect', 'Event', 'Item', 'Place', 'Detail'],
     cards: [
       {
-        id: 1,
         name: 'Wizard',
         type: 'Character',
         number: 1
       },
       {
-        id: 2,
         name: 'Dragon',
         type: 'Character',
         number: 2
       },
       {
-        id: 3,
         name: 'Ruler of the Kingdom',
         type: 'Character',
         number: 3
       },
       {
-        id: 4,
         name: 'Artifact',
         type: 'Item',
         number: 4
       },
       {
-        id: 5,
         name: 'Fae',
         type: 'Character',
         number: 5
       },
       {
-        id: 6,
         name: 'Shapeshift',
         type: 'Aspect',
         number: 6
       },
       {
-        id: 7,
         name: 'Prince',
         type: 'Character',
         number: 7
       },
       {
-        id: 8,
         name: 'Magical',
         type: 'Aspect',
         number: 8
       },
       {
-        id: 9,
         name: 'Adventurer',
         type: 'Character',
         number: 9
       },
       {
-        id: 10,
         name: 'Witch',
         type: 'Character',
         number: 10
       },
       {
-        id: 11,
         name: 'Most Desirous',
         type: 'Aspect',
         number: 11
       },
       {
-        id: 12,
         name: 'Step-Mother',
         type: 'Character',
         number: 12
       },
       {
-        id: 13,
         name: 'Knight',
         type: 'Character',
         number: 13
       },
       {
-        id: 14,
         name: 'Princess',
         type: 'Character',
         number: 14
       },
       {
-        id: 15,
         name: 'Cursed',
         type: 'Aspect',
         number: 15
       },
       {
-        id: 16,
         name: 'Chosen of the Prophecy',
         type: 'Aspect',
         number: 16
       },
       {
-        id: 17,
         name: 'Unicorn',
         type: 'Character',
         number: 17
       },
       {
-        id: 18,
         name: 'Fairy God Relative',
         type: 'Character',
         number: 18
       },
       {
-        id: 19,
         name: 'Squire',
         type: 'Character',
         number: 19
       },
       {
-        id: 20,
         name: 'Reviled',
         type: 'Aspect',
         number: 20
       },
       {
-        id: 21,
         name: 'Marriage',
         type: 'Event',
         number: 21
       },
       {
-        id: 22,
         name: 'Coming of Age',
         type: 'Event',
         number: 22
       },
       {
-        id: 23,
         name: 'Sword',
         type: 'Item',
         number: 23
       },
       {
-        id: 24,
         name: 'Crystal',
         type: 'Item',
         number: 24
       },
       {
-        id: 25,
         name: 'Tome',
         type: 'Item',
         number: 25
       },
       {
-        id: 26,
         name: 'Death',
         type: 'Event',
         number: 26
       },
       {
-        id: 27,
         name: 'Falling in Love',
         type: 'Event',
         number: 27
       },
       {
-        id: 28,
         name: 'Fight',
         type: 'Event',
         number: 28
       },
       {
-        id: 29,
         name: 'Birth',
         type: 'Event',
         number: 29
       },
       {
-        id: 30,
         name: 'Scroll',
         type: 'Item',
         number: 30
       },
       {
-        id: 31,
         name: 'Wand',
         type: 'Item',
         number: 31
       },
       {
-        id: 32,
         name: 'Coin',
         type: 'Item',
         number: 32
       },
       {
-        id: 33,
         name: 'Villager',
         type: 'Character',
         number: 33
       },
       {
-        id: 34,
         name: 'Castle',
         type: 'Place',
         number: 34
       },
       {
-        id: 35,
         name: 'Village',
         type: 'Place',
         number: 35
       },
       {
-        id: 36,
         name: 'Forest',
         type: 'Place',
         number: 36
       },
       {
-        id: 37,
         name: 'Dungeon',
         type: 'Place',
         number: 37
       },
       {
-        id: 38,
         name: 'Golden rays',
         type: 'Detail',
         number: 38
       },
       {
-        id: 39,
         name: 'Sparkling',
         type: 'Detail',
         number: 39
       },
       {
-        id: 40,
         name: 'The Stars',
         type: 'Detail',
         number: 40
@@ -256,301 +216,251 @@ export const decks = [
     cardTypes: ['Character', 'Aspect', 'Event', 'Item', 'Place', 'Detail'],
     cards: [
       {
-        id: 1,
         name: 'High schooler',
         type: 'Character',
         number: 1
       },
       {
-        id: 2,
         name: 'Secret Identity',
         type: 'Aspect',
         number: 2
       },
       {
-        id: 3,
         name: 'Alter Ego',
         type: 'Aspect',
         number: 3
       },
       {
-        id: 4,
         name: 'Transform',
         type: 'Aspect',
         number: 4
       },
       {
-        id: 5,
         name: 'Confession',
         type: 'Event',
         number: 5
       },
       {
-        id: 6,
         name: 'Battle',
         type: 'Event',
         number: 6
       },
       {
-        id: 7,
         name: 'Giant',
         type: 'Aspect',
         number: 7
       },
       {
-        id: 8,
         name: 'Kaiju',
         type: 'Character',
         number: 8
       },
       {
-        id: 9,
         name: 'Amnesia',
         type: 'Aspect',
         number: 9
       },
       {
-        id: 10,
         name: 'Animal Aspect',
         type: 'Aspect',
         number: 10
       },
       {
-        id: 11,
         name: 'Y\u014dkai',
         type: 'Character',
         number: 11
       },
       {
-        id: 12,
         name: 'Kaitou',
         type: 'Character',
         number: 12
       },
       {
-        id: 13,
         name: 'Detective',
         type: 'Character',
         number: 13
       },
       {
-        id: 14,
         name: 'Alien',
         type: 'Character',
         number: 14
       },
       {
-        id: 15,
         name: 'Ghost',
         type: 'Character',
         number: 15
       },
       {
-        id: 16,
         name: 'Robot',
         type: 'Character',
         number: 16
       },
       {
-        id: 17,
         name: 'ESPer',
         type: 'Character',
         number: 17
       },
       {
-        id: 18,
         name: 'Senpai',
         type: 'Aspect',
         number: 18
       },
       {
-        id: 19,
         name: '\u30b4\u30b4\u30b4\u30b4\u30b4',
         type: 'Detail',
         number: 19
       },
       {
-        id: 20,
         name: 'Heist',
         type: 'Event',
         number: 20
       },
       {
-        id: 21,
         name: '-dere',
         type: 'Aspect',
         number: 21
       },
       {
-        id: 22,
         name: 'Yankee',
         type: 'Character',
         number: 22
       },
       {
-        id: 23,
         name: 'Date',
         type: 'Event',
         number: 23
       },
       {
-        id: 24,
         name: 'Ninja',
         type: 'Character',
         number: 24
       },
       {
-        id: 25,
         name: 'Samurai',
         type: 'Character',
         number: 25
       },
       {
-        id: 26,
         name: 'Omiai',
         type: 'Event',
         number: 26
       },
       {
-        id: 27,
         name: 'Idol',
         type: 'Character',
         number: 27
       },
       {
-        id: 28,
         name: 'Sensei',
         type: 'Character',
         number: 28
       },
       {
-        id: 29,
         name: 'Highschool',
         type: 'Place',
         number: 29
       },
       {
-        id: 30,
         name: 'Cherry Blossoms',
         type: 'Detail',
         number: 30
       },
       {
-        id: 31,
         name: 'Shoujo Sparkles',
         type: 'Detail',
         number: 31
       },
       {
-        id: 32,
         name: 'Salaryman',
         type: 'Character',
         number: 32
       },
       {
-        id: 33,
         name: 'Office Lady',
         type: 'Character',
         number: 33
       },
       {
-        id: 34,
         name: 'Gaijin',
         type: 'Character',
         number: 34
       },
       {
-        id: 35,
         name: 'Otaku',
         type: 'Character',
         number: 35
       },
       {
-        id: 36,
         name: 'Katana',
         type: 'Item',
         number: 36
       },
       {
-        id: 37,
         name: 'Train Station',
         type: 'Place',
         number: 37
       },
       {
-        id: 38,
         name: 'Beach',
         type: 'Place',
         number: 38
       },
       {
-        id: 39,
         name: 'The City',
         type: 'Place',
         number: 39
       },
       {
-        id: 40,
         name: 'Fabulous',
         type: 'Detail',
         number: 40
       },
       {
-        id: 41,
         name: 'Sailor Fuku',
         type: 'Item',
         number: 41
       },
       {
-        id: 42,
         name: 'Pantsu',
         type: 'Item',
         number: 42
       },
       {
-        id: 43,
         name: 'Magatama',
         type: 'Item',
         number: 43
       },
       {
-        id: 44,
         name: 'Cell phone',
         type: 'Item',
         number: 44
       },
       {
-        id: 45,
         name: '\u300cFood\u300d',
         type: 'Item',
         number: 45
       },
       {
-        id: 46,
         name: 'Crowds',
         type: 'Detail',
         number: 46
       },
       {
-        id: 47,
         name: 'Onsen',
         type: 'Place',
         number: 47
       },
       {
-        id: 48,
         name: 'Park',
         type: 'Place',
         number: 48
       },
       {
-        id: 49,
         name: 'Nostalgia',
         type: 'Detail',
         number: 49
       },
       {
-        id: 50,
         name: 'Mt. Fuji',
         type: 'Detail',
         number: 50
