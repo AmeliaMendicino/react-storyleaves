ff4e3e60e53e7cca4dc396da145c765ff38bda42
Amelia Mendicino <amelia.mendicino@gmail.com>
2019-10-21 19:17:11 -0600

    Test Storage
    Testing a textbox and storage to see if it works the way I expect it to—
    that is, when the app is closed, when it opens back up again it retains
    the text that was inputted into the textbox. Seems to be working great!
    And so does eslint and prettifier! Typescript is playing nicely, too.
    I'm very pleased with this setup.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Storyleaves!</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, TextInput, View, Button, Alert, AsyncStorage } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +8,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 });
+
+function UselessTextInput(): JSX.Element {
+  const [value, onChangeText] = React.useState('Storyleaves Textbox Test');
+
+  React.useEffect(() => {
+    async function getStoryData(): Promise<void> {
+      try {
+        const storyText = await AsyncStorage.getItem('@Storyleaves:storyText');
+        if (storyText !== null) {
+          onChangeText(storyText);
+        }
+      } catch (error) {
+        Alert.alert('Error loading story data');
+      }
+    }
+    getStoryData();
+  });
+
+  return (
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={(text): void => {
+        onChangeText(text);
+        AsyncStorage.setItem('@Storyleaves:storyText', text);
+      }}
+      value={value}
+    />
+  );
+}
+
+function Separator(): JSX.Element {
+  return <View style={styles.separator} />;
+}
+
+export default function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, Storyleaves! Testing Storage</Text>
+      <Separator />
+      <UselessTextInput />
+      <Separator />
+      <Button title="Clear" onPress={(): Promise<void> => AsyncStorage.removeItem('@Storyleaves:storyText')} />
+    </View>
+  );
+}
