08d1e803f411ccad9bb57f5ea08e979431432a78
Amelia Mendicino <amelia.mendicino@gmail.com>
2019-10-22 23:02:13 -0600

    Add PanResponderExample
    Added the PanResponderExample component that creates circles that you
    can drag around. These guys are the prototypes for the cards we'll be using
    later. I'm super happy how well they turned out!!

---
+++ components/PanResponderExample.tsx
@@ -0,0 +1,120 @@
+import React, { PureComponent } from 'react';
+import { PanResponder, StyleSheet, View } from 'react-native';
+
+const CIRCLE_SIZE = 80;
+
+const styles = StyleSheet.create({
+  circle: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    backgroundColor: 'black',
+    borderRadius: CIRCLE_SIZE / 2,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+});
+
+interface PanResponderExampleProps {
+  left: number;
+  top: number;
+  color: string;
+}
+
+export default class PanResponderExample extends PureComponent<PanResponderExampleProps> {
+  circleStyles = {
+    style: {
+      backgroundColor: '',
+      left: 0,
+      top: 0,
+    },
+  };
+
+  circle: View = null;
+
+  previousLeft = 0;
+
+  previousTop = 0;
+
+  constructor(props) {
+    super(props);
+    this.previousLeft = props.left;
+    this.previousTop = props.top;
+    this.circleStyles = {
+      style: {
+        backgroundColor: props.color,
+        left: this.previousLeft,
+        top: this.previousTop,
+      },
+    };
+  }
+
+  componentDidMount(): void {
+    this.updateNativeStyles();
+  }
+
+  handleStartShouldSetPanResponder = (event, gestureState): boolean => {
+    // Should we become active when the user presses down on the circle?
+    return true;
+  };
+
+  handleMoveShouldSetPanResponder = (event, gestureState): boolean => {
+    // Should we become active when the user moves a touch over the circle?
+    return true;
+  };
+
+  handlePanResponderGrant = (event, gestureState): void => {
+    this.highlight();
+  };
+
+  handlePanResponderMove = (event, gestureState): void => {
+    this.circleStyles.style.left = this.previousLeft + gestureState.dx;
+    this.circleStyles.style.top = this.previousTop + gestureState.dy;
+    this.updateNativeStyles();
+  };
+
+  handlePanResponderEnd = (event, gestureState): void => {
+    this.unHighlight();
+    this.previousLeft += gestureState.dx;
+    this.previousTop += gestureState.dy;
+  };
+
+  highlight(): void {
+    this.circleStyles.style.backgroundColor = 'grey';
+    this.updateNativeStyles();
+  }
+
+  unHighlight(): void {
+    const { color } = this.props;
+    this.circleStyles.style.backgroundColor = color;
+    this.updateNativeStyles();
+  }
+
+  updateNativeStyles(): void {
+    if (this.circle) {
+      this.circle.setNativeProps(this.circleStyles);
+    }
+  }
+
+  /* eslint react/sort-comp: 0 */
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+    onMoveShouldSetPanResponder: this.handleMoveShouldSetPanResponder,
+    onPanResponderGrant: this.handlePanResponderGrant,
+    onPanResponderMove: this.handlePanResponderMove,
+    onPanResponderRelease: this.handlePanResponderEnd,
+    onPanResponderTerminate: this.handlePanResponderEnd,
+  });
+
+  render(): JSX.Element {
+    return (
+      <View
+        ref={(circle): void => {
+          this.circle = circle;
+        }}
+        style={styles.circle}
+        {...this.panResponder.panHandlers}
+      />
+    );
+  }
+}
--- views/GameBoard.tsx
+++ views/GameBoard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
 import StoryTextInput from '../components/StoryTextInput';
+import PanResponderExample from '../components/PanResponderExample';
+
 import { saveKey } from '../constants';
 
 const styles = StyleSheet.create({
@@ -24,6 +26,12 @@ function Separator(): JSX.Element {
 export default function GameBoard(): JSX.Element {
   return (
     <View style={styles.container}>
+      <PanResponderExample left={28} top={48} color="red" />
+      <PanResponderExample left={38} top={58} color="orange" />
+      <PanResponderExample left={48} top={68} color="yellow" />
+      <PanResponderExample left={58} top={78} color="green" />
+      <PanResponderExample left={68} top={88} color="blue" />
+      <PanResponderExample left={78} top={98} color="pink" />
       <Text>Hello, Storyleaves! Testing GameBoard PRO</Text>
       <Separator />
       <StoryTextInput />
